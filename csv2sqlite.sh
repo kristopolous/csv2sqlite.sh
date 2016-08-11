6b78bf28ecb4b84136687eb1c8e5e27832fdbb59
Chris McKenzie <kristopolous@yahoo.com>
2016-08-11 15:12:16 -0700

    ok ok another one was needed

--- csv2sqlite.sh
+++ csv2sqlite.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+[ $# -eq 0 ] && echo "Woops, I need a file." && exit -1
+
 csv="$1"
 sql="$1.db"
 rm -f "$sql"
 
-tail -n +2 "$csv" > "$csv".tmp
+tmp=`mktemp`
+tail -n +2 "$csv" > $tmp
 schema=`head -1 "$csv" | sed -e 's/[\/ "]//g'`
 
 sqlite3 "$sql" >& /dev/null << ENDL
 create table csv($schema);
 .mode csv
-.import ${csv}.tmp csv
+.import $tmp csv
 .save
 .quit
 ENDL
 
-echo "run sqlite3 $sql"
+rm -f $tmp
+echo "Done.\nsqlite3 $sql"
